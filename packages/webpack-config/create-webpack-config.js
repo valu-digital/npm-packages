e82bbd6b840f3637c27f1ab401dd6b74167a1085
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-10-05 23:03:04 +0300

    Add hot cors support

--- packages/webpack-config/create-webpack-config.js
+++ packages/webpack-config/create-webpack-config.js
@@ -5,6 +5,50 @@ const gitDate = new Date(
     execSync("git log -1 --format=%cd").toString()
 ).toISOString();
 
+function getConfig(options) {
+    return {
+        entry: {
+            main: "./src/index",
+        },
+
+        output: {
+            filename: "[name].js",
+            path: process.cwd() + "/dist",
+        },
+
+        devServer: {
+            // With dev server server files from the dist directory
+            contentBase: process.cwd() + "/dist",
+        },
+
+        resolve: {
+            // The default extensions are quite lame.
+            // the .mjs enables tree shaking for some npm modules
+            extensions: [".tsx", ".ts", ".mjs", ".jsx", ".js", ".json"],
+        },
+
+        module: {
+            rules: [
+                {test: /\.(ts|tsx|js|jsx)$/, loader: "babel-loader"},
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+
+        plugins: [
+            bundleAnalyzerPlugin(options.bundleAnalyzerPlugin),
+            htmlWebpackPlugin(options.htmlPlugin),
+            new webpack.DefinePlugin({
+                WEBPACK_GIT_DATE: JSON.stringify(gitDate),
+                WEBPACK_GIT_REV: JSON.stringify(gitRev),
+                WEBPACK_BUILD_DATE: JSON.stringify(new Date().toISOString()),
+            }),
+        ].filter(Boolean),
+    };
+}
+
 function bundleAnalyzerPlugin(activate) {
     if (!activate) return;
     const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
@@ -47,49 +91,17 @@ function createWebpackConfig(options = {}, customize) {
             process.env.NODE_ENV = args.mode;
         }
 
-        const config = {
-            entry: {
-                main: "./src/index",
-            },
+        const config = getConfig(options);
 
-            output: {
-                filename: "[name].js",
-                path: process.cwd() + "/dist",
-            },
+        const devServerPort = args.port || options.devServerPort || 8080;
+        config.devServer.port = devServerPort;
 
-            devServer: {
-                // With dev server server files from the dist directory
-                contentBase: process.cwd() + "/dist",
-            },
-
-            resolve: {
-                // The default extensions are quite lame.
-                // the .mjs enables tree shaking for some npm modules
-                extensions: [".tsx", ".ts", ".mjs", ".jsx", ".js", ".json"],
-            },
-
-            module: {
-                rules: [
-                    {test: /\.(ts|tsx|js|jsx)$/, loader: "babel-loader"},
-                    {
-                        test: /\.css$/,
-                        use: ["style-loader", "css-loader"],
-                    },
-                ],
-            },
-
-            plugins: [
-                bundleAnalyzerPlugin(options.bundleAnalyzerPlugin),
-                htmlWebpackPlugin(options.htmlPlugin),
-                new webpack.DefinePlugin({
-                    WEBPACK_GIT_DATE: JSON.stringify(gitDate),
-                    WEBPACK_GIT_REV: JSON.stringify(gitRev),
-                    WEBPACK_BUILD_DATE: JSON.stringify(
-                        new Date().toISOString()
-                    ),
-                }),
-            ].filter(Boolean),
-        };
+        if (options.hotCors) {
+            config.output.publicPath = `http://localhost:${devServerPort}/`;
+            config.devServer.headers = {
+                "Access-Control-Allow-Origin": "*",
+            };
+        }
 
         if (typeof customize === "function") {
             return customize(config, _, args);
