b8d48c6d2e2b0aa94354fe9c07e78d2e52657c0e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-10-14 12:40:15 +0300

    initial

---
+++ packages/focus-trap/src/index.ts
@@ -0,0 +1,51 @@
+import tabbable from "tabbable";
+
+export function createTrap(elements: HTMLElement[]) {
+    const containers = elements.map(el => {
+        return {
+            el,
+            hasFocus: false,
+            tabbables: tabbable(el),
+        };
+    });
+
+    let currentContainerIndex: number | null = null;
+
+    function updateCurrent(e: any) {
+        setTimeout(() => {
+            if (e.target === document.activeElement) {
+                let nextIndex = containers.findIndex(container =>
+                    container.el.contains(e.target),
+                );
+                if (nextIndex !== -1) {
+                    currentContainerIndex = nextIndex;
+                }
+            }
+        }, 1);
+    }
+
+    document.addEventListener("focusin", (e: any) => {
+        updateCurrent(e);
+        for (const container of containers) {
+            if (
+                container.el.contains(e.target) ||
+                e.target instanceof Document
+            ) {
+                return;
+            }
+        }
+
+        e.stopImmediatePropagation();
+
+        let nextIndex = 1;
+
+        if (currentContainerIndex !== null) {
+            nextIndex = (currentContainerIndex + 1) % containers.length;
+        }
+
+        const nextContainer = containers[nextIndex];
+
+        console.log("Sending to next trap", nextContainer.el);
+        nextContainer.tabbables[0].focus();
+    });
+}
