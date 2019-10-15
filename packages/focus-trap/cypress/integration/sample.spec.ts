4867e3e9c049c2708021a253ac0a97262192d4a3
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-10-15 17:39:18 +0300

    Test things

--- packages/focus-trap/cypress/integration/sample.spec.ts
+++ packages/focus-trap/cypress/integration/sample.spec.ts
@@ -1,33 +1,20 @@
 describe("basic trapping with two containers", () => {
-    it("can skip input not in a trapped container", () => {
-        cy.visit("http://localhost:8080/basic.html");
-        cy.get(".first")
-            .focus()
-            .tab()
-            .tab()
-            .focused()
-            .should("have.class", "third");
-    });
+    // it("can skip input not in a trapped container", () => {
+    //     cy.visit("http://localhost:8080/basic.html");
+    //     cy.get(".first")
+    //         .focus()
+    //         .tab()
+    //         .tab()
+    //         .focused()
+    //         .should("have.class", "third");
+    // });
 
     it("can go backwards", () => {
         cy.visit("http://localhost:8080/basic.html");
         cy.get(".third")
             .focus()
             .tab({ shift: true })
-            .tab({ shift: true })
             .focused()
-            .should("have.class", "first");
+            .should("have.class", "second");
     });
-
-    // it("sdffds", () => {
-    //     cy.visit("http://localhost:8080/basic.html");
-    //     cy.get(".first")
-    //         .focus()
-    //         .tab()
-    //         .tab()
-    //         .tab()
-    //         .tab()
-    //         .focused()
-    //         .should("have.class", "first");
-    // });
 });
