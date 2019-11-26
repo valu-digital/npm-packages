2acf6d5c6e8208cb5a3ff1dfce99519fcfa08458
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-11-26 17:48:43 +0200

    Actually export the queries

--- packages/babel-gql/src/plugin.ts
+++ packages/babel-gql/src/plugin.ts
@@ -105,6 +105,8 @@ export default function bemedBabelPlugin(
                         }),
                     ]),
                 );
+
+                qm.exportDirtyQueries();
             },
         },
     };
