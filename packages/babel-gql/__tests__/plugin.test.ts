dc60bacbfb4483a27e5328576e2a7d512834846d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-11-26 14:28:46 +0200

    Some tests

--- packages/babel-gql/__tests__/plugin.test.ts
+++ packages/babel-gql/__tests__/plugin.test.ts
@@ -46,3 +46,83 @@ test("simple transformation", () => {
     `.trim(),
     );
 });
+
+test("can handle fragments", () => {
+    const code = dedent`
+    import { gql } from "babel-gql";
+    const query = gql\`
+      fragment NameParts on Person {
+        firstName
+        lastName
+      }
+
+      query GetPerson {
+        people(id: "7") {
+          ...NameParts
+          avatar(size: LARGE)
+        }
+      }
+    \`
+    `;
+
+    const res = runPlugin(code);
+    runPlugin(code);
+
+    expect(res.code).toEqual(
+        dedent`
+        import { gql } from "babel-gql";
+        const query = gql({
+          fragments: [{
+            fragmentId: "599e02d0c3048527281d6b78058b8886cb93fca9d4f795f8383190abfcaecf5e",
+            fragmentName: "NameParts",
+            usedFragments: []
+          }],
+          queries: [{
+            queryId: "ea45bb731caccf48a370efdd98de551f9cb8c4926ddd6e440ad95a5090387716",
+            queryName: "GetPerson",
+            usedFragments: ["NameParts"]
+          }]
+        });
+        `.trim(),
+    );
+});
+
+test("fragments can use fragments", () => {
+    const code = dedent`
+    import { gql } from "babel-gql";
+    const query = gql\`
+      fragment NameParts on Person {
+        firstName
+        lastName
+      }
+
+      fragment MoreParts on Car {
+        driver {
+          ...NameParts
+          avatar(size: LARGE)
+        }
+      }
+    \`
+    `;
+
+    const res = runPlugin(code);
+    runPlugin(code);
+
+    expect(res.code).toEqual(
+        dedent`
+        import { gql } from "babel-gql";
+        const query = gql({
+          fragments: [{
+            fragmentId: "599e02d0c3048527281d6b78058b8886cb93fca9d4f795f8383190abfcaecf5e",
+            fragmentName: "NameParts",
+            usedFragments: []
+          }, {
+            fragmentId: "e600ee95689cda215e9fe3b8f65b4b4958284c2cc0d239379eda71cd427b2cc3",
+            fragmentName: "MoreParts",
+            usedFragments: ["NameParts"]
+          }],
+          queries: []
+        });
+        `.trim(),
+    );
+});
