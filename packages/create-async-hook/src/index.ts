ab93be3650fe3a0ccd32ece7db26108578c80fb4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-11-28 17:42:38 +0200

    Use deep equal

--- packages/create-async-hook/src/index.ts
+++ packages/create-async-hook/src/index.ts
@@ -2,6 +2,41 @@ import React from "react";
 
 type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
 
+/**
+ * From SO, must be perfect?
+ * https://stackoverflow.com/a/25456134/153718
+ */
+function deepEqual(x: any, y: any) {
+    if (x === y) {
+        return true;
+    } else if (
+        typeof x == "object" &&
+        x != null &&
+        typeof y == "object" &&
+        y != null
+    ) {
+        if (Object.keys(x).length != Object.keys(y).length) return false;
+
+        for (var prop in x) {
+            if (y.hasOwnProperty(prop)) {
+                if (!deepEqual(x[prop], y[prop])) return false;
+            } else return false;
+        }
+
+        return true;
+    } else return false;
+}
+
+function useDeepEqualRef<T>(o: T): T {
+    const ref = React.useRef<T>();
+
+    if (!deepEqual(o, ref.current)) {
+        ref.current = o;
+    }
+
+    return ref.current ?? o;
+}
+
 export function createAsyncHook<
     Fn extends (...args: any[]) => Promise<any>,
     State
@@ -36,52 +71,42 @@ export function createAsyncHook<
             });
         }, []);
 
-        const runtimeArgs = runtimeOptions.args ?? [];
+        const refArgs = useDeepEqualRef(runtimeOptions.args ?? []);
 
         const [state, setState] = React.useState({
             loading: true,
-            args: runtimeArgs,
+            args: refArgs,
             key: 0,
             state: options.initialState,
             refresh,
         });
 
-        const memoArgs = React.useMemo(
-            () => runtimeArgs,
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            [JSON.stringify(runtimeArgs)],
-        );
+        React.useEffect(() => {
+            setState(prev => {
+                if (prev.loading) {
+                    return prev;
+                }
 
-        React.useEffect(
-            () => {
-                setState(prev => {
-                    if (prev.loading) {
-                        return prev;
-                    }
+                return {
+                    ...prev,
+                    loading: true,
+                };
+            });
 
-                    return {
-                        ...prev,
-                        loading: true,
-                    };
-                });
+            fetcher(...refArgs).then((res: any) => {
+                setState(prevState => ({
+                    ...prevState,
+                    variables: refArgs,
+                    state: options.update(prevState.state, res, {
+                        refreshed: state.args === refArgs,
+                        previousArgs: state.args,
+                        args: refArgs,
+                    }),
 
-                fetcher(...memoArgs).then((res: any) => {
-                    setState(prevState => ({
-                        ...prevState,
-                        variables: memoArgs,
-                        state: options.update(prevState.state, res, {
-                            refreshed: state.args === memoArgs,
-                            previousArgs: state.args,
-                            args: memoArgs,
-                        }),
-
-                        loading: false,
-                    }));
-                });
-            },
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            [memoArgs, state.key],
-        );
+                    loading: false,
+                }));
+            });
+        }, [refArgs, state.key]);
 
         return state;
     } as any;
