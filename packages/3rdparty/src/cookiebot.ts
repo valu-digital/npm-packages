836eb5dda2b26069126daf31460a6eea769dd854
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-12-21 10:23:01 +0200

    Add TrackingConsnet and Cookibot integration

---
+++ packages/3rdparty/src/cookiebot.ts
@@ -0,0 +1,152 @@
+import { LazyScript } from "./lazy-script";
+import type { TrackingConsent } from "./tracking-consent";
+
+/**
+ * Type for the Cookiebot global variable
+ *
+ * https://www.cookiebot.com/en/developer/
+ */
+interface Cookiebot {
+    /**
+     * True if the user has accepted cookies. The property is read only.
+     */
+    consented: boolean;
+
+    /**
+     * True if the user has declined the use of cookies. The property is read
+     * only.
+     */
+    declined: boolean;
+
+    /**
+     * True if the user has responded to the dialog with either 'accept' or
+     * 'decline'. The property is read only.
+     */
+    hasResponse: boolean;
+
+    /**
+     * Forces the cookie consent dialog to show.
+     */
+    show(): void;
+
+    /**
+     * Withdraw my consent for this website.
+     */
+    withdraw(): void;
+
+    /**
+     * Shows the cookie consent dialog to the website user to renew or change
+     * the user's consent state.
+     */
+    renew(): void;
+}
+
+declare global {
+    interface Window {
+        // available only when the lazy script has loaded it
+        Cookiebot?: Cookiebot;
+    }
+}
+
+export function connectCookieBot(
+    trackingConsent: TrackingConsent,
+    /**
+     * The cookiebot id
+     */
+    cbid: string,
+) {
+    if (typeof window === "undefined") {
+        return;
+    }
+
+    const CookiebotScript = new LazyScript({
+        name: "cookiebot",
+        src: "https://consent.cookiebot.com/uc.js",
+        initialize: () => {
+            if (!window.Cookiebot) {
+                throw new Error(
+                    "@valu/3rdparty: Cookiebot did not load properly",
+                );
+            }
+            return window.Cookiebot;
+        },
+        mutate: (el) => {
+            el.dataset.blockingmode = "manual";
+            el.dataset.cbid = cbid;
+            el.id = "Cookiebot";
+        },
+    });
+
+    /**
+     * Disable all blocking for the cookiebot crawler so it can see the trackers
+     *
+     * https://support.cookiebot.com/hc/en-us/community/posts/360010100774-UserAgent-Need-to-know-its-CookieBot-to-avoid-Blocking-unknown-Scanner
+     *
+     *    'Yes. Since the last update, you could now identify Cookiebot through
+     *    the user agent string and it will contain "Cookiebot".'
+     *
+     * User agent seems to be like this:
+     *
+     *  Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko; compatible; Cookiebot/1.0; +http://cookiebot.com/) Chrome/86.0.4240.183 Safari/537.36
+     *
+     */
+    if (/cookiebot/i.test(window.navigator.userAgent)) {
+        trackingConsent.consent();
+        LazyScript.disableAllBlocking();
+        CookiebotScript.now();
+    }
+
+    window.addEventListener(
+        "CookiebotOnAccept",
+        () => {
+            if (window.Cookiebot?.hasResponse) {
+                trackingConsent.consent();
+            }
+        },
+        false,
+    );
+
+    window.addEventListener(
+        "CookiebotOnDecline",
+        () => {
+            if (window.Cookiebot?.hasResponse) {
+                trackingConsent.decline();
+            }
+        },
+        false,
+    );
+
+    trackingConsent.onEvent((event) => {
+        switch (event) {
+            // No need to handle these beacuse this is just a one away binding.
+            // Eg. we only sync the state from the cookiebot to TrackingConsent
+            case "consented":
+            case "declined": {
+                return;
+            }
+
+            case "forget": {
+                CookiebotScript.now();
+
+                return CookiebotScript.promise().then((cb) => {
+                    cb.renew();
+                });
+            }
+
+            case "request-prompt": {
+                CookiebotScript.now((cb) => {
+                    // If cookiebot has no response we must not call cb.show()
+                    // because it will open the dialog automatically on load. If
+                    // we call it as well it will flash.
+                    if (cb.hasResponse) {
+                        cb.show();
+                    }
+                });
+                return;
+            }
+        }
+
+        // TypeScript trick to ensure all events are handled
+        const _: never = event;
+    });
+}
---
+++ packages/3rdparty/src/tracking-consent.ts
@@ -0,0 +1,167 @@
+const STATUSES = ["not-given", "declined", "consented"] as const;
+
+interface ConsentResponse {
+    status: typeof STATUSES[number];
+    date: string | undefined;
+}
+
+type AsAny<T> = {
+    [P in keyof T]?: any;
+};
+
+type TrackingConsentEvent =
+    | "request-prompt"
+    | "consented"
+    | "declined"
+    | "forget";
+
+export interface TrackingConsentEventHandler {
+    (event: TrackingConsentEvent): undefined | void | Promise<any>;
+}
+
+function debug(...args: any[]) {
+    console.log(...args);
+}
+
+export class TrackingConsent {
+    response: ConsentResponse;
+
+    storeKey: string;
+
+    eventHandlers: TrackingConsentEventHandler[];
+
+    constructor(options?: { key?: string }) {
+        this.eventHandlers = [];
+        this.storeKey = options?.key ?? "valu-tracking-response";
+        this.response = {
+            status: "not-given",
+            date: undefined,
+        };
+
+        this.read();
+    }
+
+    init() {
+        if (this.response.status === "not-given") {
+            this.showPrompt();
+        }
+        this.sendGTMDatalayerEvent();
+    }
+
+    sendGTMDatalayerEvent() {
+        if (typeof window === "undefined") {
+            return;
+        }
+        const anyWindow = window as any;
+
+        const dl: { event: string }[] = anyWindow.dataLayer ?? [];
+        // create one if it did not exists
+        anyWindow.dataLayer = dl;
+
+        dl.push({ event: "valu-tracking-response-" + this.response.status });
+    }
+
+    consent() {
+        if (this.response.status === "consented") {
+            return;
+        }
+
+        debug("Got tracking consent");
+        this.response = {
+            status: "consented",
+            date: new Date().toISOString(),
+        };
+        this.save();
+        this.emit("consented");
+        this.sendGTMDatalayerEvent();
+    }
+
+    decline() {
+        if (this.response.status === "declined") {
+            return;
+        }
+
+        debug("Declined consent");
+        this.response = {
+            status: "declined",
+            date: new Date().toISOString(),
+        };
+        this.save();
+        this.emit("declined");
+        this.sendGTMDatalayerEvent();
+    }
+
+    forget() {
+        debug("Forget consent");
+        this.response = {
+            status: "not-given",
+            date: undefined,
+        };
+        this.save();
+        return this.emitPromise("forget").then(() => {
+            this.showPrompt();
+        });
+    }
+
+    emit(event: TrackingConsentEvent) {
+        void this.emitPromise(event);
+    }
+
+    emitPromise(event: TrackingConsentEvent) {
+        return Promise.all(this.eventHandlers.map((fn) => fn(event)));
+    }
+
+    onEvent(cb: TrackingConsentEventHandler) {
+        this.eventHandlers.push(cb);
+        return () => {
+            this.off(cb);
+        };
+    }
+
+    off(cb: TrackingConsentEventHandler) {
+        const index = this.eventHandlers.findIndex((f) => f === cb);
+        this.eventHandlers.splice(index, 1);
+    }
+
+    showPrompt() {
+        this.emit("request-prompt");
+    }
+
+    read() {
+        if (typeof window === "undefined") {
+            return;
+        }
+
+        const raw = window.localStorage.getItem(this.storeKey) ?? "{}";
+
+        let data: AsAny<ConsentResponse> = {};
+
+        try {
+            data = JSON.parse(raw);
+        } catch {}
+
+        if (data.status && STATUSES.includes(data.status)) {
+            this.response.status = data.status;
+        }
+
+        if (data.date) {
+            this.response.date = data.date;
+        }
+    }
+
+    save() {
+        if (typeof window === "undefined") {
+            return;
+        }
+
+        if (this.response.status === "not-given") {
+            window.localStorage.removeItem(this.storeKey);
+            return;
+        }
+
+        window.localStorage.setItem(
+            this.storeKey,
+            JSON.stringify(this.response),
+        );
+    }
+}
