be2b9210212b9aa1c43f63398ae516f02bf8c834
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-12-23 08:42:33 +0200

    Avoid react hydration issues in the hooks

--- packages/3rdparty/src/react-lazy-script.ts
+++ packages/3rdparty/src/react-lazy-script.ts
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import type { LazyScript } from "./lazy-script";
 
-export function useLazyScript(script: LazyScript<any>): LazyScript["state"] {
-    const [state, setState] = useState<LazyScript["state"]>(script.state);
+export function useLazyScript(
+    script: LazyScript<any>,
+    initial?: LazyScript["state"],
+): LazyScript["state"] {
+    // Start with the "idle" status...
+    const [state, setState] = useState<LazyScript["state"]>(initial ?? "idle");
 
     useEffect(() => {
+        // ...and only in the first mount read the actual status to avoid React
+        // SSR hydration issues
+        setState(script.state);
+
         return script.onStateChange((newState) => {
             setState(newState);
         });
--- packages/3rdparty/src/react-tracking-consent.ts
+++ packages/3rdparty/src/react-tracking-consent.ts
@@ -3,12 +3,18 @@ import type { TrackingConsent } from "./tracking-consent";
 
 export function useTrackingConsent(
     tc: TrackingConsent,
+    initial?: TrackingConsent["response"]["status"],
 ): TrackingConsent["response"]["status"] {
+    // Start with the "not-given" status...
     const [state, setState] = useState<TrackingConsent["response"]["status"]>(
-        tc.response.status,
+        initial ?? "not-given",
     );
 
     useEffect(() => {
+        // ...and only in the first mount read the actual status to avoid React SSR
+        // hydration issues
+        setState(tc.response.status);
+
         // Event is always emitted when the reponses changes. This will be
         // emitted on other events too but React can detect the change from the
         // string and avoid useless rendering automatically
