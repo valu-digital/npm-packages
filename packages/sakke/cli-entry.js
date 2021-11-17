cd59e7b529f5aefb8100c925b0590cdbe139b962
Esa-Matti Suuronen <esa-matti@suuronen.org>
2021-11-17 15:26:14 +0200

    Handle cli promise errors

--- packages/sakke/cli-entry.js
+++ packages/sakke/cli-entry.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-require("./dist/cli").cli(process.argv);
+require("./dist/cli")
+    .cli(process.argv)
+    .catch((error) => {
+        console.error(error);
+        process.exit(2);
+    });
