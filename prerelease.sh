ab62791df914f290c711d273bf588f1bce3850b7
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-11-18 14:12:56 +0200

    Use @valu/npm-tools in release scripts

--- prerelease.sh
+++ prerelease.sh
@@ -2,30 +2,4 @@
 
 set -eu
 
-
-if [ "${1:-}" != "" ]; then
-    cd "packages/$1"
-fi
-
-pkg="$(basename $(pwd))"
-
-if [ "$(git rev-parse --show-toplevel)" = "$(pwd)" ]; then
-    echo "prerelease.sh must be run from the packge dir"
-fi
-
-if [ ! -f package.json ]; then
-    echo
-    echo "No package.json at $(pwd)"
-    echo
-    exit 1
-fi
-
-
-if [ "$(git status . --porcelain)" != "" ]; then
-    echo
-    echo "Dirty git. Commit changes"
-    echo
-    exit 1
-fi
-
-git push -f origin HEAD:release/$pkg/prerelease
+exec "$(git rev-parse --show-toplevel)/packages/npm-tools/scripts/valu-npm-prerelease"
